09b06efa8e844042ea4d353023aabc94ff0f38ad
Austin Donovan <iskarr@Austins-MacBook-Pro.local>
2023-08-31 14:21:24 -0600

    working login/logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
   const session = await getAuthSession();
@@ -19,8 +20,8 @@ const Navbar = async () => {
         </Link>
 
         {/* Search bar */}
-        {session ? (
-          <p>You are Logged in</p>
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
         ) : (
           <Link href="/sign-in" className={buttonVariants()}>
             Sign In
